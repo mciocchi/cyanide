d42db21409cf1d8abbe03ec8731aa46a3612bdd3
mciocchi <mciocchi@gmail.com>
2016-02-16 01:43:53 -0500

    - adjusted test to reflect renaming that happened during an earlier refac

--- tests/tests.el
+++ tests/tests.el
@@ -1,4 +1,4 @@
-(defun foccur-case-sensitive-test (func)
+(defun cyanide-case-sensitive-test (func)
   "case | arg  | case-fold-search | func output | test output
         |      |                  |             | (nil = fail)
    -----+------+------------------+-------------+-------------
@@ -6,9 +6,9 @@
    2    | asdF | t                | nil         | nil
    3    | asdf | nil              | nil         | nil
    4    | asdF | nil              | nil         | nil
-   5    | asdf | t                | nil         | t  
-   6    | asdF | t                | 3           | t  
-   7    | asdf | nil              | t           | t  
+   5    | asdf | t                | nil         | t
+   6    | asdF | t                | 3           | t
+   7    | asdf | nil              | t           | t
    8    | asdF | nil              | 3           | t
 "
   (let ((failed ""))
@@ -18,14 +18,14 @@
         (let ((failed (if (funcall func "asdf")
                           (concat
                            failed
-                           "foccur-case-sensitive-test [FAILED]: "
+                           "cyanide-case-sensitive-test [FAILED]: "
                            "False positive with case-fold-search t "
                            "and lowercase arg.\n"))))
           ;; fail case 2, pass case 6
           (let ((failed (if (not (funcall func "asdF"))
                             (concat
                              failed
-                             "foccur-case-sensitive-test [FAILED]: "
+                             "cyanide-case-sensitive-test [FAILED]: "
                              "False negative with case-fold-search t "
                              "and uppercase arg.\n"))))
             ;; fail case 3, pass case 7
@@ -33,26 +33,26 @@
               (let ((failed (if (not (funcall func "asdf"))
                                 (concat
                                  failed
-                                 "foccur-case-sensitive-test [FAILED]: "
+                                 "cyanide-case-sensitive-test [FAILED]: "
                                  "False negative with case-fold-search nil "
                                  "and lowercase arg.\n"))))
                 ;; fail case 4, pass case 8
                 (let ((failed (if (not (funcall func "asdF"))
                                   (concat
-                                   failed "foccur-case-sensitive-test "
+                                   failed "cyanide-case-sensitive-test "
                                    "[FAILED]: False negative with "
                                    "case-fold-search nil and "
                                    "uppercase arg.\n"))))
                   (if failed (print failed)
-                    (print "foccur-case-sensitive-test [PASSED]\n")) ;; else
+                    (print "cyanide-case-sensitive-test [PASSED]\n")) ;; else
                   (if failed nil ;if failed return false
                     t)))))))))) ;else return true
 
 ;; Return true only if all tests pass.
 ;; New tests need to be added here.
 (defun run-tests ()
-  (let ((case-sensitive-test-result (foccur-case-sensitive-test
-                                     'foccur-case-sensitive)))
+  (let ((case-sensitive-test-result (cyanide-case-sensitive-test
+                                     'cyanide-case-sensitive)))
     (and case-sensitive-test-result)))
 
 (run-tests)
