f3486c9d06763d6718eaebed125c9431fe3879f0
mciocchi <mciocchi@gmail.com>
2016-05-29 21:45:46 -0400

    - new class: cyanide-task extends cyanide-menu-item-function
    - cyanide-task-builder

--- src/cyanide.el
+++ src/cyanide.el
@@ -137,6 +137,13 @@
       "Class that represents functions stored in a
        `cyanide-menu'.")
 
+    ; Tasks are nothing more than cyanide-menu-functions for now, but it is
+    ; likely that they will need to provide additional functionality soon.
+    (defclass cyanide-task (cyanide-menu-function)
+      ()
+      "Class that represents repetitive jobs that need to be
+       manually executed.")
+
     (defun cyanide-menu-builder (kwargs)
       "Constructor for `cyanide-menu'."
       (cyanide-kwargobj-builder 'cyanide-menu
@@ -147,6 +154,13 @@
     (defun cyanide-menu-function-builder (kwargs)
       "Constructor for `cyanide-menu-function'."
       (cyanide-kwargobj-builder 'cyanide-menu-function
+                                kwargs
+                                '(:id :display-name :func)
+                                'cyanide-menu-item-collection))
+
+    (defun cyanide-task-builder (kwargs)
+      "Constructor for cyanide-tasks."
+      (cyanide-kwargobj-builder 'cyanide-task
                                 kwargs
                                 '(:id :display-name :func)
                                 'cyanide-menu-item-collection))
