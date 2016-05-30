7881fe6b8ea3b5942dbfc09f78149f7026d359d5
mciocchi <mciocchi@gmail.com>
2016-05-30 02:41:57 -0400

    - Major revamp, modularized views into separate packages

---
+++ src/views/cyanide-views.el
@@ -0,0 +1,19 @@
+;; This file is part of CyanIDE.
+;;
+;; CyanIDE is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; CyanIDE is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with CyanIDE.  If not, see <http://www.gnu.org/licenses/>.
+
+(require 'cyanide-default-menu-with-tasks)
+(require 'cyanide-default-view)
+(require 'cyanide-elisp-view)
+(provide 'cyanide-views)
