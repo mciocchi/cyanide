37601e37184ae43701ef7abfee460b9296521152
Matt Ciocchi <mciocchi@gmail.com>
2017-11-17 18:02:53 +0800

    fixed regression from disabling views inside of a view
    * :enable and :disable on views replaced with :teardown-hook and :load-hook
      from cyanide-hookable
    
    * projects are no longer required to have a :default-view
    
    * removed overly verbose logging for :load-hook and :teardown-hook
    
    * removed cyanide-hoook-executor, no longer used
    
    * cyanide-default-view is not useful and not default. Blew it away!

--- lib/cy-tk/controller/cyanide-loader.el
+++ lib/cy-tk/controller/cyanide-loader.el
@@ -15,6 +15,48 @@
 
 (require 'cyanide-globals)
 
+(defun cyanide-try-load-dotfile (dotfile memo)
+  (condition-case err
+      (load dotfile)
+    ('error (message (concat "error while loading dotfile: "
+                             dotfile
+                             "\nerror: "
+                             (format "%s" err))))))
+
+(defun cyanide-load-toplevel (memo toplevel)
+  (if (and (file-exists-p toplevel)
+             (directory-name-p toplevel))
+    (let ((memo-and-subdirs (cons memo (directory-files toplevel))))
+      (reduce (lambda (memo subdir)
+                (when (and (file-directory-p (concat toplevel subdir))
+                           (not (equal "." subdir))
+                           (not (equal ".." subdir))
+                           (file-exists-p (concat toplevel subdir))
+                           (file-exists-p (concat
+                                           toplevel
+                                           subdir
+                                           "/"
+                                           cyanide-project-config-file-name)))
+                  ;; also check for dupe project ids at project construction
+                  ;; time, throw away and raise alert message if one already
+                  ;; exists. This should be part of cyanide-identifiable
+                  (cyanide-try-load-dotfile (concat
+                                             toplevel
+                                             subdir
+                                             "/"
+                                             cyanide-project-config-file-name)
+                                            memo))
+                memo)
+              memo-and-subdirs))
+    ; else
+    (message (concat "could not load projects from toplevel which is not a "
+                     "directory: " toplevel))))
+
+(defun cyanide-load-dotfiles ()
+  (let ((memo-and-toplevels (cons '() cyanide-project-toplevel-directories)))
+    (reduce (lambda (memo elt) (cyanide-load-toplevel memo elt))
+            memo-and-toplevels)))
+
 (defun cyanide-get-project-directories-from-toplevel ()
   (let ((project-directories '()))
     (mapcar (lambda (toplevel)
